883188e93c32567cb248ef42a72dbcd1a29a6fda
ktallaris <netcitizen@email.com>
2026-08-19 15:37:21 +0300

    remove from socage the math-skills folder

--- socage/math-skills/converters/standard-deviation.go
+++
@@ -1,9 +0,0 @@
-package converters
-
-import "math"
-
-func StandardDeviation(nums []float64, variance float64) float64 {
-
-	stdDev := math.Sqrt(variance)
-	return stdDev
-}
--- socage/math-skills/go.mod
+++
@@ -1,3 +0,0 @@
-module math-skills
-
-go 1.24.9
